5d8e265aa27eec2fed0a420c87482b0016892a58
Robin Baby <robierobz1308@gmail.com>
2023-04-17 09:33:38 +1200

    Add JS tech blog

---
+++ blog/Untitled-1.js
@@ -0,0 +1,13 @@
+if(trafficLight==='Red'){
+  //instruction to stop
+  }
+  else if(trafficLight==='Yellow'){
+  //instruction to slow down and be prepared to stop
+  }
+  else if(trafficLight==='Green'){
+    //the light is green and traffic can proceed
+    }
+  else{
+  //Error
+}
+  
